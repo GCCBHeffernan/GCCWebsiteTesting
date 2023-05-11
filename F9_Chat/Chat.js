c8e494862fcde25623817d510e7ca73c73f95a96
GCCBHeffernan <123589716+GCCBHeffernan@users.noreply.github.com>
2023-05-11 12:34:13 +0100

    Update Chat.js
    Sent from Justin

--- F9_Chat/Chat.js
+++ F9_Chat/Chat.js
@@ -5,12 +5,12 @@ var f9_hiddenfields;
 if(typeof mw_customer_data === 'undefined'){
 	console.log("User Logged In");
 	f9_visiblefields = {"en":{"messenger":{"customText":{"inputPlaceholderBlocked":"Complete the form, then press \"Start\"..."}},"systemMessages":{"transferredToParticipant":"The chat has been transferred to {name}.","transferredToGroup":"That chat has been transferred to group {group}."},"captureFields":[{"k":"name","l":"First Name","p":"Enter your first name..."},{"k":"email","l":"Email Address","p":"Enter your email..."},{"k":"Caller.PhoneNumber","l":"Phone Number","p":"Enter your phone number..."},{"k":"zip","l":"Zip Code","p":"Enter your zip code..."},{"k":"Question","l":"Question","p":"What can we help you with today?"}]}};
-	f9_hiddenfields = [{"k":"campaign","v":"Chat - Banyan Hill"},{"k":"Caller.Customer Number","v":""}];
+	f9_hiddenfields = [{"k":"campaign","v":"Chat - Money And Markets"},{"k":"Caller.Customer Number","v":""}];
 }
 else {
 	console.log("User Not Logged In");
 	f9_visiblefields = {"en":{"messenger":{"customText":{"inputPlaceholderBlocked":"Complete the form, then press \"Start\"..."}},"systemMessages":{"transferredToParticipant":"The chat has been transferred to {name}.","transferredToGroup":"That chat has been transferred to group {group}."},"captureFields":[{"k":"Caller.PhoneNumber","l":"Phone Number","p":"Enter your phone number..."},{"k":"Question","l":"Question","p":"What can we help you with today?"}]}};
-	f9_hiddenfields = [{"k":"campaign","v":"Chat - Banyan Hill"},{"k":"name","v":mw_customer_data["agora_customer_firstname"]},{"k":"email","v":mw_customer_data["agora_customer_email"]},{"k":"zip","v":mw_customer_data["agora_customer_zip"]},{"k":"Caller.Customer Number","v":mw_customer_data["agora_customer_number"]}]
+	f9_hiddenfields = [{"k":"campaign","v":"Chat - Money And Markets"},{"k":"name","v":mw_customer_data["agora_customer_firstname"]},{"k":"email","v":mw_customer_data["agora_customer_email"]},{"k":"zip","v":mw_customer_data["agora_customer_zip"]},{"k":"Caller.Customer Number","v":mw_customer_data["agora_customer_number"]}]
 
 }
 
@@ -21,5 +21,5 @@ F9.Chat.Wrapper.init({
 	languages: {"enabled":false},
 	l10n: f9_visiblefields,    
 	prepopulatedFields: f9_hiddenfields,    
-	messenger: {"integrationId":"6329b66aa8113300f3063f8d","soundNotificationEnabled":true,"transcriptPrintingEnabled":true,"menuItems":{"imageUpload":true,"fileUpload":true,"shareLocation":true},"embedded":false,"businessName":"Banyan Hill Publishing","businessIconUrl":"https://cdn.banyanhill.com/wp-content/uploads/2021/05/05110622/cropped-180x180_BanyanFavicon-192x192.jpg","browserStorage":"sessionStorage","fixedHeader":false,"displayStyle":"button","customColors":{"brandColor":"ff6600","conversationColor":"ff6600","actionColor":"ff6600"}}
+	messenger: {"integrationId":"6329b66aa8113300f3063f8d","soundNotificationEnabled":true,"transcriptPrintingEnabled":true,"menuItems":{"imageUpload":true,"fileUpload":true,"shareLocation":true},"embedded":false,"businessName":"Money & Markets","businessIconUrl":"https://moneyandmarkets.com/wp-content/uploads/2020/05/MAM_Favicon_2.png","browserStorage":"sessionStorage","fixedHeader":false,"displayStyle":"button","customColors":{"brandColor":"2D3041","conversationColor":"2D3041","actionColor":"2D3041"}}
 });
